2c9de5adc8b5a5914d01471618f45977b6d6db8d
Chase Sullins <jcsullins222@yahoo.com>
2020-01-31 11:45:05 -0600

    completed Daily Journal 5

--- src/scripts/data.js
+++ src/scripts/data.js
@@ -1,8 +1,15 @@
-const getJournalEntries = () => {
-    const getMovies = "http://localhost:3000/entries";
-    fetch(getMovies)
-      .then(resp => resp.json())
-      .then(entriesFromAPI => {
-        renderJournalEntries(entriesFromAPI);
-      });
-  };
+// const getJournalEntries = () => {
+//     const getMovies = "http://localhost:3000/entries";
+//     fetch(getMovies)
+//       .then(resp => resp.json())
+//       .then(entriesFromAPI => {
+//         renderJournalEntries(entriesFromAPI);
+//       });
+//   };
+
+const API = {
+    getJournalEntries () {
+        return fetch("http://localhost:3000/entries")
+            .then(response => response.json())
+    }
+}
--- src/scripts/journal.js
+++ src/scripts/journal.js
@@ -1,1 +1,6 @@
-getJournalEntries()
+/*
+    Main application logic that uses the functions and objects
+    defined in the other JavaScript files.
+*/
+
+API.getJournalEntries().then(renderJournalEntries)
